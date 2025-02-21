a11ed45caa73720b2bd892262c0ece40174eff13
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-02-21 15:45:28 -0500

    markdwon fix

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,21 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/styles");
 
+  // Add markdown support for components
+  const markdownIt = require("markdown-it");
+  const markdownLib = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  });
+  
+  // Add markdown filter for use in templates
+  eleventyConfig.addFilter("markdown", function(value) {
+    return markdownLib.render(value);
+  });
+  
+  eleventyConfig.setLibrary("md", markdownLib);
+
   return {
     dir: {
       input: "src",
